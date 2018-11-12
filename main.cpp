cd425056f2da3b9afd685441ed38b9ca149ff537
Fluturel Adrian <fluturel.adrian@gmail.com>
2018-11-12 22:51:00 +0200

    // comments

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "bitmap.cpp"
 #include "kmeans.hpp"
 
+// Returns random number in range [startRange, endRange] of integral type
 template<typename IntegralType>
 IntegralType IntRandomEngine<IntegralType>::getRandomNumber() {
 		std::uniform_int_distribution<IntegralType> distribution(startRange, endRange); 
@@ -15,14 +16,17 @@ IntegralType IntRandomEngine<IntegralType>::getRandomNumber() {
 		return ret;
 	}
 
+// Converts a Pixel to a Centroid implicitly if needed (downcast operator)
 Pixel::operator Centroid() {
 	return Centroid(r, g, b);
 }
 
+// Computes squared Euclidean distance between two points
 long computeDistance(const Pixel &a, const Pixel &b) {
 	return (a.r - b.r)*(a.r - b.r) + (a.g - b.g)*(a.g - b.g) + (a.b - b.b)*(a.b - b.b);
 }
 
+// Calculates distance from pixel to every chosen centroid, returns the minimum
 long chooseClosestCentroid(Pixel &pixel, const Centroid* centers, const short &centersChosen) {
 	long min = std::numeric_limits<long>::max();
 	for (short centerIndex = 0; centerIndex < centersChosen; centerIndex++)
@@ -35,6 +39,7 @@ long chooseClosestCentroid(Pixel &pixel, const Centroid* centers, const short &c
 	return min;
 }
 
+// Chooses centroids with the k-means++ initialization method
 void chooseCentroids(Pixel* pixels, const long &numberOfPixels, const short &clusters) {
 	IntRandomEngine<long> engine(0, numberOfPixels);
 
@@ -43,8 +48,9 @@ void chooseCentroids(Pixel* pixels, const long &numberOfPixels, const short &clu
 
 	short chosenCentroids = 1l;
 	for (short clusterno = 1; clusterno < clusters; clusterno++) {
-		double *distances = new double[numberOfPixels];
+		double *distances = new double[numberOfPixels]; // Distances from every pixel to its nearest centroid
 		for (long px = 0l; px < numberOfPixels; px++) {
+			// Now the coordinates for the closest centroid are stored in pixels[px]
 			distances[px] = chooseClosestCentroid(pixels[px], centroids, chosenCentroids);
 		}
 
@@ -54,11 +60,13 @@ void chooseCentroids(Pixel* pixels, const long &numberOfPixels, const short &clu
 	delete[] centroids;
 }
 
+// Image metadata gets printed to stdout
 void printDetails(const BitmapImage& image) {
 	std::cout << "The image has " << image.getWidth() << "x" << image.getHeight() << " pixels." << std::endl;
 	std::cout << "The bit depth is " << image.getBitDepth() << " bits." << std::endl;
 }
 
+// Reads the file, calls the main k-means methods and writes the file back to disk
 void startKmeans(const char* filename) {
 	BitmapImage image(filename);
 	image.readHeader();
@@ -66,7 +74,7 @@ void startKmeans(const char* filename) {
 	image.readArray();
 
 	long numberOfPixels = image.getWidth()*image.getHeight();
-	Pixel *pixels = new Pixel[numberOfPixels];
+	Pixel *pixels = new Pixel[numberOfPixels]; // Helper class for working with pixels
 	for (long index = 0; index < numberOfPixels*3; index += 3) {
 		pixels[index/3].r = image[index];
 		pixels[index/3].g = image[index + 1];
@@ -75,6 +83,7 @@ void startKmeans(const char* filename) {
 	
 	chooseCentroids(pixels, numberOfPixels, 50);
 
+	// Clean up useless buffers and write to disk
 	delete[] pixels;
 	image.writeToFile("results/marbles.bmp");
 }
@@ -88,6 +97,7 @@ int main(int argc, char** argv) {
 
 	++argv;
 
+	// Parse and try to open any file given in the argument
 	for (int iter = 0; iter < argc - 1; iter++) {
 		try {
 		startKmeans(argv[iter]);
