3b3c7dd69007cb73a15e5060a6fef320d4e8068d
Fluturel Adrian <fluturel.adrian@gmail.com>
2018-11-12 03:39:59 +0200

    choosing centroids

--- main.cpp
+++ main.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include <limits>
+#include <vector>
+#include <random>
 #include <unistd.h>
 
 #include "bitmap.cpp"
+
+template <class IntegralType>
+class IntRandomEngine {
+	IntegralType startRange, endRange;
+	std::default_random_engine generator;
+public:
+	IntRandomEngine(const long long &startRange, const long long &endRange = std::numeric_limits<IntegralType>::max())
+		: startRange(startRange), endRange(endRange) {}
+
+	IntegralType getRandomNumber() {
+		std::uniform_int_distribution<IntegralType> distribution(startRange, endRange); 
+		IntegralType ret = distribution(this->generator);
+		distribution.reset();
+		return ret;
+	}
+};
+
+struct Centroid;
+
+struct Pixel {
+	char r, g, b;
+	Pixel() : r(0), g(0), b(0) {}
+	Pixel(const char &r, const char &g, const char &n) : r(r), g(g), b(b) {}
+	operator Centroid();
+};
+
+struct Centroid : public Pixel {
+	Centroid() : Pixel() {}
+	Centroid(const char &r, const char &g, const char &b) : Pixel(r, g, b) {}
+};
+
+Pixel::operator Centroid() {
+	return Centroid(r, g, b);
+}
+
+long computeDistance(const Pixel &a, const Pixel &b) {
+	return (a.r - b.r)*(a.r - b.r) + (a.g - b.g)*(a.g - b.g) + (a.b - b.b)*(a.b - b.b);
+}
+
+void chooseCentroids(Pixel* pixels, const long &numberOfPixels, const short &clusters) {
+	IntRandomEngine<long> engine(0, numberOfPixels);
+
+	Centroid *centroids = new Centroid[clusters];
+	centroids[0] = pixels[engine.getRandomNumber()];
+
+	long chosenCentroids = 1l;
+	for (short clusterno = 0l; clusterno < clusters; clusterno++)
+		for (long index = 0l; index < chosenCentroids; index++) {
+
+	}
+	
+	delete[] centroids;
+}
 
 void printDetails(const BitmapImage& image) {
 	std::cout << "The image has " << image.getWidth() << "x" << image.getHeight() << " pixels." << std::endl;
@@ -14,12 +70,18 @@ void startKmeans(const char* filename) {
 	printDetails(image);
 	image.readArray();
 
-	for (int index = 0; index < image.getHeight(); index++) {
-		image[index*image.getWidth()] = 255;
-		image[index*image.getWidth()+ 1] = 255;
-		image[index*image.getWidth()+ 2] = 210;
+	long numberOfPixels = image.getWidth()*image.getHeight();
+	Pixel *pixels = new Pixel[numberOfPixels];
+	for (long index = 0; index < numberOfPixels*3; index += 3) {
+		pixels[index/3].r = image[index];
+		pixels[index/3].g = image[index + 1];
+		pixels[index/3].b = image[index + 2];
 	}
-	image.writeToFile("results/marbles_TEST.bmp");
+	
+	chooseCentroids(pixels, numberOfPixels, 255);
+
+	delete[] pixels;
+	image.writeToFile("results/marbles.bmp");
 }
 
 int main(int argc, char** argv) {
